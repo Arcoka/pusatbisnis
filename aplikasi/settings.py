2398534b69a988cfe64bffa9f560c2ec133e2bfb
Dy_miracle <onemiracle07@gmail.com>
2025-12-25 12:54:52 +0700

    Fix Railway Deploy

--- aplikasi/settings.py
+++ aplikasi/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o7790i2&@9=1b64e1t1+q9)cha82la-zq%ij8z()jt6jf%ex(h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DJANGO_DEBUG', default=True, cast=bool)
+DEBUG = config('DJANGO_DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
